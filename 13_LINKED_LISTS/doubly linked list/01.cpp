87894864f4354e2423d5b9fe0062b6f6cbe78cc7
Shekhar Bhide <shekharbhide036@gmail.com>
2023-05-09 15:34:40 +0530

    dll

---
+++ 13_LINKED_LISTS/doubly linked list/01.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+
+    Node()
+    {
+        this->data = 0;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+
+    Node(int data)
+    {
+        this->data = data;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void insertAtHead(Node *&head, Node *&tail, int data)
+{
+    if (head == NULL) // LL is empty
+    {
+        Node *newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        // LL is Non-Empty
+        Node *newNode = new Node(data);
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+    }
+}
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int getLength(Node *head)
+{
+    int len = 0;
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        temp = temp->next;
+        len++;
+    }
+    return len;
+}
+
+int main()
+{
+
+    Node *first = new Node(10);
+    Node *second = new Node(20);
+    Node *third = new Node(30);
+
+    first->next = second;
+    second->prev = first;
+    second->next = third;
+    third->prev = second;
+    third->next = NULL;
+
+    print(first);
+    cout << endl;
+    Node *head = first;
+    Node *tail = third;
+    insertAtHead(head, tail, 100);
+
+    print(head);
+    return 0;
+}
